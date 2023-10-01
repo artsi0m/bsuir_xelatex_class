bdd091aa1a489d5369e0abc3ca7fb6bc7ff6b8ff
artsi0m <karakin2000@gmail.com>
2023-10-01 18:49:46 +0300

    Simple build script

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$document = "main"
+$file = "{0}.tex" -f $document
+$iso_date = Get-Date -format "yyyy-MM-dd"
+$pdf_source = "{0}.pdf" -f $document
+$pdf_to_send = "Karakin5semCoursework-{0}.pdf" -f $iso_date
+
+# Why it runs for 3 times:
+# https://ru.wikibooks.org/wiki/LaTeX/BibLaTeX
+xelatex $file
+biber $document
+xelatex $file
+xelatex $file
+
+Copy-Item -Path $pdf_source -Destination $pdf_to_send
